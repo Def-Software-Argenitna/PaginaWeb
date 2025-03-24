552474983905a3be5187de4746646027ca2357e5
def-software <jpablojavier.peralta@gmail.com>
2025-03-24 07:28:29 -0300

    Actualización en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from 'react';
 import './Css/App.css';
 import ContactForm from './Components/ContactForm';
@@ -46,39 +45,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-=======
-//import { useState } from 'react'
-import reactLogo from './assets/react.svg';
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from '@vercel/speed-insights/react';
-//import viteLogo from '/web.jpg'
-import './App.css'
-import './estilos/fondo.css'
-import './estilos/estilos.css'
-
-function App() {
-  //const [count, setCount] = useState(0)
-
-  return (
-    <>
-    <div>
-        <div className='background'>
-          <h1 className="animated zoomInDown" color='blue'>Página en construcción</h1>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <Analytics/>
-        <SpeedInsights/>
-      </div>                           
-      </>
-      
-  )
-}
-
-export default App
->>>>>>> 549bbef2c67276e01338c55e4665aab4e8120aac
